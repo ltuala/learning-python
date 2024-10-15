42ccfeaaa8b7f7c91d04c0af3ba77f2be18383cc
Lyndon Tuala <lyndontuala28@gmail.com>
2024-10-15 04:03:44 +0000

    d4 checkpoint

--- todo_app/main.py
+++ todo_app/main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, or exit: ").strip()
+    user_action = input("Type add, show, edit, or exit: ").strip()
     
     match user_action:
         case 'add':
@@ -10,6 +10,11 @@ while True:
         case 'show':
             for todo in todos:
                 print(todo)
+        case 'edit':
+            number = int(input("Number of the todo to edit: "))
+            idx = number - 1
+            new_todo = input("Enter new todo: ")
+            todos[idx] = new_todo
         case 'exit':
             break
         case _:
