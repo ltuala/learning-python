0947561448480294f15d573425da0aa919770e36
Lyndon Tuala <lyndontuala28@gmail.com>
2024-11-12 10:15:22 +0800

    d16 checkpoint 2

--- todo_app/gui.py
+++ todo_app/gui.py
@@ -1,8 +1,11 @@
-import FreeSimpleGUI.window
 from functions import get_todos, write_todos
 
-import FreeSimpleGUI
+import FreeSimpleGUI as sg
 
-window = FreeSimpleGUI.Window('My To-Do App', layout=[""])
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="Enter todo")
+add_button = sg.Button("Add")
+
+window = sg.Window('My To-Do App', layout=[[[label], [input_box, add_button]]])
 window.read()
 window.close()
