0c9c1f0e291b5b904b3950d2706db9aa5de919f4
Lyndon Tuala <lyndontuala28@gmail.com>
2024-11-20 10:34:37 +0800

    d24 checkpoint

--- app3-pdf_template/main.py
+++ app3-pdf_template/main.py
@@ -2,6 +2,7 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
@@ -10,6 +11,26 @@ for index, row in df.iterrows():
 
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
-    pdf.cell(w=0, h=12, txt=row["topic"], align="L", ln=1)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    for y in range(20, 298, 10):    
+        pdf.line(10, y, 200, y)
+
+    # Set the footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1)
+
+    for i in range(row["Pages"] -  1):
+        pdf.add_page()
+
+        # Set the footer
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1)
+        
+        for y in range(20, 298, 10):    
+            pdf.line(10, y, 200, y)
 
 pdf.output("output.pdf")
