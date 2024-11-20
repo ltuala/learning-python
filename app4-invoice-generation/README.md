835d2271089e5e14179ab3cd0111a30ce4adffd8
Lyndon Tuala <lyndontuala28@gmail.com>
2024-11-20 11:43:11 +0800

    d25 checkpoint

---
+++ app4-invoice-generation/README.md
@@ -0,0 +1,2 @@
+# What is this project
+This is an app that create PDF invoices out of Excel files.
---
+++ app4-invoice-generation/exercise/main.py
@@ -0,0 +1,17 @@
+import glob
+from fpdf import FPDF
+from pathlib import Path
+
+filepaths = glob.glob("text_files/*.txt")
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+for filepath in filepaths:
+    pdf.add_page()
+
+    filename = Path(filepath).stem
+    title = str(filename).title()
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=title, ln=1)
+
+pdf.output(f"PDFs/output.pdf")
