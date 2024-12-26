a7e9a0ca33dd51c71756286c9d448d34767a2122
Lyndon Tuala <lyndontuala28@gmail.com>
2024-12-26 17:04:59 +0800

    d43 checkpoint

---
+++ app11-hotel-booking/main2.py
@@ -0,0 +1,75 @@
+import pandas
+
+df = pandas.read_csv("hotels.csv", dtype={"id": str})
+
+
+class Hotel:
+    watermark = "The Real Estate Company"
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
+    
+    def book(self):
+        """Book a hotel by changing its availability to no"""
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
+    
+    def available(self):
+        """Check if the hotel is available"""
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
+    
+    @classmethod
+    def get_hotel_count(cls, data):
+        return len(data)
+
+
+class ReservationTicket:
+    def __init__(self, customer_name, hotel_object):
+        self.customer_name = customer_name
+        self.hotel = hotel_object
+
+    def generate(self):
+        content = f"""
+        Thank you for your reservation!
+        Here are your booking data:
+        Name: {self.the_customer_name}
+        Hotel name: {self.hotel.name}
+        """
+        return content
+    
+    @property
+    def the_customer_name(self):
+        name = self.customer_name.strip()
+        name = name.title()
+        return name
+    
+    @staticmethod
+    def convert(amount):
+        return amount * 1.2
+
+
+hotel1 = Hotel(hotel_id="188")
+hotel2 = Hotel(hotel_id="134")
+
+print(hotel1.available())
+print(hotel1.name)
+print(hotel2.name)
+
+print(hotel1.watermark)
+print(hotel2.watermark)
+
+print(Hotel.watermark)
+
+print(Hotel.get_hotel_count(data=df))
+print(hotel1.get_hotel_count(data=df))
+
+ticket = ReservationTicket(customer_name="john smith ", hotel_object=hotel1)
+print(ticket.the_customer_name)
+print(ticket.generate())
+
+converted = ReservationTicket.convert(10)
+print(converted)
